0756e225e03f94f30a87e68c6dcc010bfd49b409
Jacob McCracken <mccrackenjacob@gmail.com>
2020-07-07 17:53:37 +0000

    added notations for constant time functions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const boxes = [0, 1, 2, 3, 4, 5];
 
 function logFirstTwoBoxes(boxes) {
-  console.log([0]);
-  console.log([1]);
+  console.log([0]); //O(1)
+  console.log([1]); //O(1)
 }
 
-logFirstTwoBoxes(boxes);
+logFirstTwoBoxes(boxes); //O(2)
+
+// O stands for operations value in the parentheses is the number of operations
